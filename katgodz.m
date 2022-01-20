d5648a03bbb61a0150e552912e4f7edbbdb0ea75
307326 <94767028+307326@users.noreply.github.com>
2022-01-20 17:41:55 +0100

    Delete katgodz.m

--- katgodz.m
+++
@@ -1,9 +0,0 @@
-function [t] = katgodz(y,d,m,h,lambda,alfa)
-jd = julday(y,m,d,0); %dni
-g = GMST(jd); %stopnie
-UT1 = h*1.002737909350795; %godziny
-%obliczenie czasu gwiazdowego(w stopniach)
-S = UT1*15 + lambda + g;
-%obliczenie kąta godzinnego(w stopniach)
-t = S - alfa*15;
-end
